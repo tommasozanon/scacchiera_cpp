394f3b0668dd389c9615f034fef358164871580d
tommasozanon <tommaso@pop-os.localdomain>
2021-12-22 18:06:01 +0100

    3

--- pieces/Piece.h
+++ pieces/Piece.h
@@ -7,6 +7,10 @@ class Piece {
     Piece(const Piece&) = delete;
     Piece& operator=(Piece const&);
     char to_string() { return repr; }
+    void set_pos(short pos[2]) {
+        position[0] = pos[0];
+        position[1] = pos[1];
+    };
 
   protected:
     std::vector<short> position;
@@ -17,6 +21,8 @@ class Piece {
     Piece(){};
     virtual std::vector<std::vector<short>> get_allowed_moves() = 0;
     virtual std::vector<short> get_position() = 0;
+
+    //~Piece(){};
 };
 
 #endif
