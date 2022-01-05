63927ca2632db9ff1248999ab6ce6090111d0c21
tommaso <tommaso@pop-os.localdomain>
2022-01-05 16:01:44 +0100

    qualche cosa

--- special_moves.h
+++ special_moves.h
@@ -8,24 +8,24 @@
 
 /*Method is_check: verifies check conditions
  *input:	-vector of shorts  (where you verify if it is check)
-				-matrix of pieces (game chessboard)
-				-pieces' colour
+            -matrix of pieces (game chessboard)
+            -pieces' colour
  *output: true if is check, false otherwise
 */
 bool is_check(std::vector<short> pos, const std::vector<std::vector<std::shared_ptr<Piece>>>& board, short my_color);
 
 /*Method is_castling: verifies castling conditions
  *input:	-vector of shorts (king's position)
-				-vector of shorts (rook's position)
-				-matrix of pieces (game chessboard)
- *output: true if you can castle, false otherwise 
+                        -vector of shorts (rook's position)
+                        -matrix of pieces (game chessboard)
+ *output: true if you can castle, false otherwise
 */
 bool is_castling(std::vector<short> pos_re, std::vector<short> pos_torre, const std::vector<std::vector<std::shared_ptr<Piece>>>& board);
 
 /*Method is_discovery_check: verifies if a piece can be move without discover his king
  *input:	- a Board (game's chessboard)
-				- vector of shorts (pos1, the position of the piece you want to move)
-				- vector of shorts (pos2, the position where the piece is moved)
+                                - vector of shorts (pos1, the position of the piece you want to move)
+                                - vector of shorts (pos2, the position where the piece is moved)
  *output: true if you can move, false otherwise
  *
 */
