fbcb9d49facd6608e63a6a22ed0ebae6ae03f952
Giulio <giulio.piccolin@gmail.com>
2022-01-17 11:45:50 +0100

    torre e donna sistemati del tutto

--- main.cpp
+++ main.cpp
@@ -79,31 +79,15 @@ int main() {
     std::cout << "castling: " << is_castling(pos1, pos2, b.board) << std::endl;
 
     // test alfiere, donna, re e torre (con scacco di scoperta e arrocco)
-    short n = 3;
+    short n = 4;
     short m = 4;
-    pos1[0] = 6;
-    pos1[1] = 1;
+    pos1[0] =7;
+    pos1[1] = 3;
     pos2[0] = n;
-    pos2[1] = m+2;
-	std::vector<short> pos3{0, 1};
-	std::vector<short> pos3a{0, 2};
-	std::vector<short> pos3b{0, 5};
-	std::vector<short> pos3c{0, 6};
-	std::vector<short> pos4{n-1, m};
-	std::vector<short> pos4a{n, m};
-	std::vector<short> pos4b{n, m+1};
-	std::vector<short> pos4c{n, m-1};
-	std::vector<short> pos5{7, 2};
-	std::vector<short> pos6{n-1, m+1};
+    pos2[1] = m;
     b.move(pos1, pos2);
-	b.move(pos3, pos4);
-	b.move(pos3a, pos4a);
-	b.move(pos3b, pos4b);
-	b.move(pos3c, pos4c);
-	b.move(pos5, pos6);
     b.print();
-    std::vector<std::vector<short>> movess = get_moves(b, b.board[0][4]);
-	std::cout<<"Mosse del re in basso:"<<"\n";
+    std::vector<std::vector<short>> movess = get_moves(b, b.board[n][m]);
     for (int i = 0; i < movess.size(); i++) {
         std::cout << "( " << movess[i][0] + 1 << ", " << (char)(movess[i][1] + 1+96) << ")" << std::endl;
     }
