1a93e06cb66f0b5a6fbe5af02c7d7b829b05aa44
Giulio <giulio.piccolin@gmail.com>
2022-01-21 22:47:23 +0100

    replay

--- replay.cpp
+++ replay.cpp
@@ -1,18 +1,18 @@
-#include "Board.h"
-#include "pieces/Piece.h"
-
 #include <string>
+#include <vector>
+
+#include <unistd.h>
+
 #include <fstream>
 #include <iostream>
 using namespace std;
+
+#include "Board.h"
 
 // si esegue il file con:
 // 	./replay v logReplay.txt  ----> si vuole l'output a video con il print della scacchiera
 // 	./replay f logReplay.txt fileReplay.txt ----> si vuole l'output a file della serie di mosse
 int main(int args, char* argv[]){
-	//argv[1] = f oppure v
-	//argv[2] = log
-	//argv[3] esiste solo se (argv[1]=f) e = file replay
 	
 	if (args<=2){
 		cout<<"Invalid input:"<<"\n";
@@ -36,6 +36,31 @@ int main(int args, char* argv[]){
 			cout<<"write './replay v [log_file_name]' to see the board and the sequence of the moves"<<"\n";
 			return 0;
 		}
+		
+		Board chessboard;
+		chessboard.print();
+		string logFile_name = argv[2];
+		ifstream in {logFile_name};
+		if (!in){
+			cout<<"can't open input file "<<logFile_name<<"\n";
+			return 0;
+		}
+			
+		string line;
+		while (getline(in, line)){
+			sleep(2);
+			int row1 = line.at(1)-49;
+			int column1 = tolower(line.at(0))-97;
+			int row2 = line.at(4)-49;
+			int column2 = tolower(line.at(3))-97;
+			
+			vector<short> initial_pos {row1, column1};
+			vector<short> final_pos {row2, column2};
+			
+			chessboard.move(initial_pos, final_pos);
+			chessboard.print();
+		}
+		in.close();
 	}
 	else{
 		if (args!=4){
@@ -43,10 +68,27 @@ int main(int args, char* argv[]){
 			cout<<"write './replay v [log_file_name] [file_output_replay_name]' to print in the specified file the sequence of the moves"<<"\n";
 			return 0;
 		}
+		
+		string logFile_name = argv[2];
+		string replayFile_name = argv[3];
+		
+		ifstream in {logFile_name};
+		if (!in){
+			cout<<"can't open input file "<<logFile_name<<"\n";
+			return 0;
+		}
+		
+		ofstream out {replayFile_name};
+		if (!out){
+			cout<<"can't open output file "<<logFile_name<<"\n";
+			return 0;
+		}
+		string line;
+		while (getline(in, line)){
+			out<<line<<"\n";
+		}
+		in.close();
 	}
-	
 	
 	return 1;
 }
-
-#include "Board.cpp"
