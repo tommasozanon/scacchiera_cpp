78d942d2c985c55a0d72a6b375127ece46684254
Giulio <giulio.piccolin@gmail.com>
2022-01-21 18:01:42 +0100

    tryuhb

--- replay.cpp
+++ replay.cpp
@@ -1,4 +1,5 @@
 #include "Board.h"
+#include "pieces/Piece.h"
 
 #include <string>
 #include <fstream>
@@ -12,18 +13,17 @@ int main(int args, char* argv[]){
 	//argv[1] = f oppure v
 	//argv[2] = log
 	//argv[3] esiste solo se (argv[1]=f) e = file replay
-	/*
+	
 	if (args<=2){
 		cout<<"Invalid input:"<<"\n";
 		cout<<"write './replay v [log_file_name]' to see the board and the sequence of the moves"<<"\n";
-		cout<<"write './replay v [log_file_name] [file_output_replay_name]' to print in the specified file the sequence of the moves"<<"\n";
+		cout<<"write './replay f [log_file_name] [file_output_replay_name]' to print in the specified file the sequence of the moves"<<"\n";
 	
 		return 0;
 	}
-	*/
-	string InputString1 = *argv[1] +"";
-	cout<<InputString1<<"\n";
-	cout<<InputString1<<"\n";
+	
+	string InputString1 = argv[1];
+	
 	if (InputString1!="v" && InputString1!="f"){
 		cout<<"Invalid input: insert 'v' or 'f' as first string in the command line"<<"\n";
 		
@@ -34,7 +34,6 @@ int main(int args, char* argv[]){
 		if (args!=3){
 			cout<<"Invalid input:"<<"\n";
 			cout<<"write './replay v [log_file_name]' to see the board and the sequence of the moves"<<"\n";
-		
 			return 0;
 		}
 	}
@@ -42,11 +41,12 @@ int main(int args, char* argv[]){
 		if (args!=4){
 			cout<<"Invalid input:"<<"\n";
 			cout<<"write './replay v [log_file_name] [file_output_replay_name]' to print in the specified file the sequence of the moves"<<"\n";
-		
 			return 0;
 		}
 	}
 	
-	//ifstream fin("argv[2]");
+	
 	return 1;
 }
+
+#include "Board.cpp"
